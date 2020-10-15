63a5c487ba1ca1ea86606df0a8b5d805a2430bd8
Kaunain26 <kaunainnesar26@gmail.com>
2020-10-15 19:45:41 +0530

    initial commit

--- app/src/main/java/com/knesarcreation/attendanceapp/StudentsPastAttendance.kt
+++ app/src/main/java/com/knesarcreation/attendanceapp/StudentsPastAttendance.kt
@@ -1,7 +1,6 @@
 package com.knesarcreation.attendanceapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,11 +14,6 @@ import kotlinx.android.synthetic.main.activity_std_past_attendance.*
 class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnItemClickListener {
     private var id: Int? = 0
     private var showAlertDialog: Boolean = false
-
-    /* var incrementedPresent = 0
-     var decrementAbsent = 0
-     var incrementAbsent = 0
-     var decrementPresent = 0*/
     private var mAdapter: AdapterStdPastAttendance? = null
     private var mDatabase: Database? = null
     private var mStdPastList = mutableListOf<StudentPastAttendance>()
@@ -38,10 +32,7 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
         txtDateTimeStdPastAttend.text = dateTime
         txtSubNameFragmentPast.text = subName
 
-        Log.d(
-            "studentPastHistory",
-            "${mDatabase?.mDao()?.getDatesTimesAndStudentHist(id!!.toInt())!!}"
-        )
+
         for (i in mDatabase?.mDao()?.getDatesTimesAndStudentHist(id!!.toInt())!!) {
             mStdPastList = i.studentPastAttendance
         }
@@ -63,23 +54,6 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
 
     private fun saveAttendanceBtn() {
         imgSaveBtnPastAttend.setOnClickListener {
-            /* val builder = AlertDialog.Builder(this)
-             builder.setTitle("Save Changes" as CharSequence)
-             builder.setMessage("Do you want to save changes to attendance?" as CharSequence)
-             builder.setPositiveButton(
-                 "Save"
-             ) { dialog, _ ->
-                 savePastAttendance()
-                 finish()
-                 dialog.dismiss()
-             }
-             builder.setNegativeButton(
-                 "Don't save"
-             ) { dialog, _ ->
-                 dialog.dismiss()
-                 finish()
-             }
-             builder.show()*/
             savePastAttendance()
             Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
             finish()
@@ -95,8 +69,7 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
 
     private fun savePastAttendance() {
         for (k in positionList) {
-            /* for (tempList in tempStudDetailsList) {
-                 if (k == tempList.position) {*/
+
             /*Saving: The changes made in past attendance with respect to position which was added to positionList*/
             if (mStdPastList[k].isChecked) {
                 mDatabase?.mDao()
@@ -122,9 +95,6 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
         positionList.add(Position)
         if (myViewHolder?.checkbox?.isChecked!!) {
             for (i in mDatabase?.mDao()?.getStudentDetails(mStdPastList[Position].stdUsn!!)!!) {
-                /* val tempStudentDetails = TempStudentDetails(Position, i.absent - 1, i.present + 1)
-                 tempStudDetailsList.add(tempStudentDetails)
-                 mDatabase?.mDao()?.insertTempStudentDetails(tempStudentDetails)!!*/
                 mDatabase?.mDao()
                     ?.updateStudentPresentStatus(i.present + 1, mStdPastList[Position].stdUsn!!)
 
@@ -134,15 +104,6 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
         } else {
 
             for (i in mDatabase?.mDao()?.getStudentDetails(mStdPastList[Position].stdUsn!!)!!) {
-                /*   val tempStudentDetails = TempStudentDetails(Position, i.absent + 1, i.present - 1)
-                   tempStudDetailsList.add(
-                       TempStudentDetails(
-                           Position,
-                           i.absent + 1,
-                           i.present - 1
-
-                       )
-                   )*/
                 mDatabase?.mDao()
                     ?.updateStudentAbsentStatus(i.absent + 1, mStdPastList[Position].stdUsn!!)
 
@@ -151,24 +112,4 @@ class StudentsPastAttendance : AppCompatActivity(), AdapterStdPastAttendance.OnI
             }
         }
     }
-
-    /*  private fun backPressed() {
-          if (showAlertDialog) {
-              val builder = AlertDialog.Builder(this)
-              builder.setTitle("Alert!!" as CharSequence)
-              builder.setMessage("Do you want to save changes to attendance?" as CharSequence)
-              builder.setPositiveButton("Save") { dialog, _ ->
-                  savePastAttendance()
-                  finish()
-                  dialog.dismiss()
-              }
-              builder.setNegativeButton("No") { dialog, _ ->
-                  dialog.dismiss()
-                  finish()
-              }
-              builder.show()
-          } else {
-              super.onBackPressed()
-          }
-      }*/
 }
